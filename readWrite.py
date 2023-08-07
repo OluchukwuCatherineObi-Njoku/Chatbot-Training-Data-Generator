dd0171b61b39769bb80ef552aab4a2dcb0d060d5
Oluchukwu Catherine Obi-Njoku <oobi-njoku@myseneca.ca>
2023-08-06 23:27:40 -0400

    All

---
+++ readWrite.py
@@ -0,0 +1,75 @@
+from collections import deque
+import json
+
+def readFile(file):
+    with open(file, "r") as f:
+        lines = f.readlines()
+        return lines
+
+def create_an_intent(questions, answers, file_delimiter):
+    intent = {
+      "tag": "question",
+      "patterns": [],
+      "responses": []
+    }
+
+    # i = len(questions)
+    # print(i, "questions")
+    # while i > 0:
+    #     item = questions.popleft()
+    #     if(item != file_delimiter):
+    #         intent["patterns"].append(item)
+    #     else:
+    #         i = len(answers)
+    #         print(i, "answers")
+    #         while i > 0:
+    #             item = answers.popleft()
+    #             if(item != file_delimiter):
+    #                 intent["responses"].append(item)
+    #             else:
+    #                 i = 0
+    # return intent
+    
+    q = True
+    a = True
+
+    while q or a:
+        q_item = questions.popleft() if (len(questions) > 0 and q == True) else file_delimiter
+        a_item = answers.popleft() if (len(answers) > 0 and a == True) else file_delimiter
+        if q_item != file_delimiter:
+            intent["patterns"].append(q_item.strip())
+        else:
+            q = False
+        
+        if a_item != file_delimiter:
+            intent["responses"].append(a_item.strip())
+        else:
+            a = False
+    return intent
+
+
+
+def refine_read_file_data(train_Qs, train_As, file_delimiter):
+    questions = deque(train_Qs)
+    answers = deque(train_As)
+    intents = []
+    while len(questions) > 0 and len(answers) > 0:
+        intents.append(create_an_intent(questions,answers,file_delimiter))
+    return intents
+        
+
+
+def main():
+    questions = readFile('training_texts/Training text.txt')
+    answers = readFile('training_texts/answers_training.txt')
+
+    # write to json file extracted data
+    with open("bom_deus.json", "w") as f:
+        json.dump(refine_read_file_data(questions,answers,'\n'), f, indent=4)
+
+
+#Using the special variable
+#__name__
+
+if __name__ == "__main__":
+    main()
